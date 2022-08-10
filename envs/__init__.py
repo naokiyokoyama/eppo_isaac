87f441c55d5a2ecc1a5cb15b2647891ffed8626a
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-08-09 23:46:21 -0400

    forgot to push init

--- envs/__init__.py
+++ envs/__init__.py
@@ -2,6 +2,8 @@ from isaacgymenvs.tasks import isaacgym_task_map
 
 from .ant import AntV2
 from .humanoid import HumanoidV2
+from .anymal_terrain import AnymalTerrainV2
 
 isaacgym_task_map[AntV2.orig_name] = AntV2
 isaacgym_task_map[HumanoidV2.orig_name] = HumanoidV2
+isaacgym_task_map[AnymalTerrainV2.orig_name] = AnymalTerrainV2
